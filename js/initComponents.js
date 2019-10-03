152230048645a0f3f0219d8ec475cd4f528f1e13
Rubén Díaz <rediaz1808@gmail.com>
2019-10-03 17:22:35 -0500

    Initial commit

---
+++ js/initComponents.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+  /* M.AutoInit(); */
+
+  /* Dropdown Init */
+  $(".dropdown-trigger").dropdown({
+    coverTrigger: false
+  });
+  $(".dropdown-trigger-nested").dropdown({
+    hover: true,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    gutter: ($('.dropdown-content').width() * 3) / 2.5 + 5, // Spacing from edge
+    belowOrigin: false, // Displays dropdown below the button
+    alignment: 'left' // Displays dropdown with edge aligned to the left of button
+  });
+
+  /* Sidenav Init */
+  $('.sidenav').sidenav();
+
+
+  /* Carousel Init */
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    duration: 300
+  });
+
+
+
+
+});
